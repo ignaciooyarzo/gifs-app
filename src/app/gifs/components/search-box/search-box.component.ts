d19cc14cf55be1c56c14617407834154829d97f8
Ignacio Oyarzo <ignaciooyarzo826@gmail.com>
2024-05-13 13:38:43 -0400

    agregando el formato css

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,34 @@
+
+
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs.service';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+  <h5>Buscar:</h5>
+  <input type="text"
+    class="form-control"
+    placeholder="Busar gifs..."
+    (keyup.enter)="searchTag()"
+    #txtTagInput
+    >`
+})
+
+export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor( private GifsService: GifsService) { }
+
+  // searchTag( newTag: string ){
+    searchTag(){
+      const newTag = this.tagInput.nativeElement.value;
+
+      this.GifsService.searchTag(newTag);
+
+      this.tagInput.nativeElement.value = '';
+  }
+
+}
